f70eefba04669de956df2291c6e6e6ab659d5126
Taylor Chen <s68175@gmail.com>
2020-07-18 00:06:07 +0800

    Add show multi-restaurant

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 
+const restaurantList = require('./restaurant.json')
+
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -15,7 +17,7 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 // start and listen on Express server
